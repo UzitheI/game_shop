3fa99a8be2d33117c0d9b10fc7034e9db41bcab9
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-04 18:10:54 +0545

    structure

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header(){
+    return(
+        <div>
+            <img src="" alt="" />
+            <div>
+                search
+            </div>
+            <div>
+                <a href=""></a>
+                <a href=""></a>
+            </div>
+        </div>
+    )
+}
