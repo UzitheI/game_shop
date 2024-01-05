e69b35d154b032f88af6aca819974460ab1bb289
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-05 21:08:20 +0545

    completed Header temporarily

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,15 @@
-import logo from '../src/constants/image.js'
+import {image} from "../constants/image"
+
 export default function Header(){
     return(
-        <header className="flex border border-black pt-8 pb-10 mb-0 w-full  justify-between px-12">
-            <img src="../src/assets/logo.png" alt="logo" className="border border-black w-auto h-20 pointer rounded-lg "/>
+        <header className="flex border border-black pt-8 pb-10 mb-0 w-full  justify-between px-12 bg-black text-white ">
+            <img src={image[0].image} alt={image[0].alt} className="border border-black w-auto h-20 pointer rounded-lg "/>
             <div>
                <input type="text" className="border border-black rounded-lg text-white " />
             </div>
-            <div className="flex justify-between ">
+            <div className="flex justify-between space-x-4 ">
                 <a href="">add</a>
-                <div className="border border-black "></div>
+                <div className="border border-white mb-10 "></div>
                 <a href="">cart</a>
             </div>
         </header>
