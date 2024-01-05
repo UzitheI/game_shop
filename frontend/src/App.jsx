b260680279434ce793a30695cc5b1694dc20be51
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-05 21:09:57 +0545

    Added Body

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header"
-
+import Body from "./components/Body"
 export default function App(){
   return(
 
     <div>
     <Header/>
+    <Body/>
   </div>
     )
 }
