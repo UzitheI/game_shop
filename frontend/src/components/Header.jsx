8a57c8fbf0fbde8e4f2d34e57dfa77f0db697b67
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-05 22:41:13 +0545

    Added Search and Toggle Input width

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
 import {image} from "../constants/image"
 
 export default function Header(){
+    const[inputText,setInputText]=useState('');
+    const[isOpen, setIsOpen]=useState(false);
+
+    const toggleWidth=(width)=>{
+        setIsOpen(!isOpen);
+    };
     return(
         <header className="flex pt-8 pb-10 mb-0 w-full  justify-between px-12  text-white ">
             <img src={image[0].image} alt={image[0].alt} className="border border-black w-auto h-20 pointer rounded-lg "/>
-            <div>
-               <input type="text" className="border border-black rounded-lg text-white " />
+            <div className="relative">
+               <input type="text" className={`border border-black rounded-lg text-black p-2 w-80 transition-all ${isOpen?"w-96":""}`} 
+               placeholder="Search your game.."
+               value={inputText}
+               onClick={toggleWidth}
+               onChange={(e)=>setInputText(e.target.value)}/>
+               <button className="absolute right-4 top-5 transform -translate-y-1/2 px-4 bg-white text-black rounded-lg" onClick={toggleWidth}>Search</button>
             </div>
             <div className="flex justify-between space-x-4 ">
                 <a href="">add</a>
