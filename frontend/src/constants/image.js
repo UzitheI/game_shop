5afc2a2716d1a27496a43d142f3af1b5d7807283
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-05 22:06:56 +0545

    removed background image component

--- frontend/src/constants/image.js
+++ frontend/src/constants/image.js
@@ -1,15 +1,10 @@
 import logo from "../assets/logo.png"
-import background from "../assets/background.mp4"
 
 export const image=[
     {
         image:logo,
         alt:"logo-image",
     },
-    {
-        image:background,
-        alt:"background-body"
-    }
     
 ];
 
