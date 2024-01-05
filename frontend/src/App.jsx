f6e1655d278c6c957f002c5656a00b1c62ccb01e
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-05 22:41:38 +0545

    added new background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Body from "./components/Body";
-import background from "./assets/background.mp4"; // Import the MP4 video file
+import background2 from "./assets/background2.mp4"; // Import the MP4 video file
 
 export default function App() {
   return (
@@ -13,7 +13,7 @@ export default function App() {
         playsInline
         className="absolute top-0 left-0 object-cover w-full h-full"
       >
-        <source src={background} type="video/mp4" />
+        <source src={background2} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
