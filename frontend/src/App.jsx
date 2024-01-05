16920a43e8cd121ee93733a777e3e89a833793d1
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-05 22:07:22 +0545

    Added background image to the whole App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-import Header from "./components/Header"
-import Body from "./components/Body"
-export default function App(){
-  return(
+import Header from "./components/Header";
+import Body from "./components/Body";
+import background from "./assets/background.mp4"; // Import the MP4 video file
 
-    <div>
-    <Header/>
-    <Body/>
-  </div>
-    )
+export default function App() {
+  return (
+    <div className="relative h-screen">
+      {/* Video Background */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute top-0 left-0 object-cover w-full h-full"
+      >
+        <source src={background} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      {/* Content Overlay */}
+      <div className="relative z-10">
+        <Header />
+        <Body />
+      </div>
+    </div>
+  );
 }
