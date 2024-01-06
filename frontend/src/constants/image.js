707b27ac7f3716e4c89fb8c83ec0a26a3b044a45
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-06 19:01:55 +0545

    added new image

--- frontend/src/constants/image.js
+++ frontend/src/constants/image.js
@@ -1,9 +1,14 @@
 import logo from "../assets/logo.png"
+import github from "../assets/github.png"
 
 export const image=[
     {
         image:logo,
         alt:"logo-image",
+    },
+    {
+        image:github,
+        alt:"github",
     },
     
 ];
