e2bedb19f055a23e2020e48e37c057f454e1891e
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-06 20:33:57 +0545

    links for body

---
+++ frontend/src/constants/links.js
@@ -0,0 +1,11 @@
+
+export const links=[
+    {
+        path:"https://github.com/UzitheI/game_shop",
+        name:"github",
+    },
+    {
+        path:"https://store.steampowered.com/",
+        name:"steam"
+    }
+];
