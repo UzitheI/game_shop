69759f8643fcef069a18838b259880b964765b85
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-06 20:33:06 +0545

    data for nav

---
+++ frontend/src/constants/data.js
@@ -0,0 +1,9 @@
+export const datalist = [
+    { text: "Play Dice", value: 10 },
+    { text: "Added games", value: 20 },
+    { text: "Last 30 days", value: 30 },
+    { text: "Popular in 2023", value: 30 },
+    { text: "Best of the year", value: 30 },
+    { text: "All time top", value: 30 },
+
+  ];
