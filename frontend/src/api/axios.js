aa67c24646c465e00485048218331903b77de1e6
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-08 20:18:31 +0545

    axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,1 +1,24 @@
 //baseURL: "https://api.rawg.io/api/"
+
+import axios, {AxiosRequestConfig} from "axios";
+
+const axiosInstance =axios.create({
+    baseURL:"https://api.rawg.io/api",
+    params:{
+        key:"c14a43ddf59e42bba18d3744d8c470cb"
+    },
+});
+
+function Axios(){
+    const getData =()=>{
+        axios.get('https://rawg.io/api/games')
+        .then(res=>{
+            console.log(res)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }
+}
+
+export default axios;
