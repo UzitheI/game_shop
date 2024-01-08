e7dbb3ede1f4cb682353b04c2b4d2905346ed0c8
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-08 06:42:31 +0545

    new router dependencies

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Add from "../src/components/Add.jsx";
+
+const router=createBrowserRouter([
+  {path:"/",
+   element:<App/>,
+  },
+  {
+    path:"add",
+    element:<Add/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router}>
+
     <App />
+    </RouterProvider>
+   
   </React.StrictMode>,
 )
