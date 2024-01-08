38e2b3831f46c6bf0d3f305b45bac2ab1679e598
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-08 20:19:40 +0545

    completed PlayDice

--- frontend/src/components/Right.jsx
+++ frontend/src/components/Right.jsx
@@ -5,7 +5,7 @@ export default function Right() {
     <div className="bg-white bg-opacity-15  text-black w-1/6 rounded-3xl py-6 ">
       <h1 className="flex justify-center text-3xl bg-white rounded-lg">Quick Navigation</h1>
       {datalist.map((item, index) => (
-        <Link to="/last30days" key={index} className="bg-white text-black text-lg mt-4 rounded-lg flex align-middle justify-center w-2/3 ml-10 hover:bg-blue-600 cursor-pointer pop-out   ">
+        <Link to="/PlayDice" key={index} className="bg-white text-black text-lg mt-4 rounded-lg flex align-middle justify-center w-2/3 ml-10 hover:bg-blue-600 cursor-pointer pop-out   ">
           {`${item.text}`}
         </Link>
       ))}
