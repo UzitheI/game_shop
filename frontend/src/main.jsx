d2b9516f65aca37398c476a47789fb9da453db05
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-08 15:30:07 +0545

    defined Last30days

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Add from "../src/components/Add.jsx";
+import Last30days from "./components/Last30days.jsx";
 
 const router=createBrowserRouter([
   {path:"/",
@@ -12,7 +13,11 @@ const router=createBrowserRouter([
   {
     path:"add",
     element:<Add/>
-  }
+  },
+  {
+    path:"last30days",
+    element:<Last30days/>
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
