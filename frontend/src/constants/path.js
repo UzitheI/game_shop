1200e36707665286e9c7ff2b23a817521c255e2c
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-09 22:15:52 +0545

    final

---
+++ frontend/src/constants/path.js
@@ -0,0 +1,8 @@
+export const Path=[
+        path='/PlayDice',
+        path= '/Added game',
+        path='/Last30days',
+        path='/PopularIn2023',
+        path='/BestofTheYear',
+        path='/Alltime',
+];
