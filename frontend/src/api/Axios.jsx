a44692374ad72aaba7c39d735082fb73d8553caa
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-09 22:12:52 +0545

    changes in axios

---
+++ frontend/src/api/Axios.jsx
@@ -0,0 +1,20 @@
+// import axios  from "axios";
+
+// const Axios =(id)=>{
+    
+    
+//     axios.get(`https://rawg.io/api/games/${id}?token&key=${apiKey}`)
+//         .then(res=>{
+//             console.log(res);
+//             setName(res.data.name);
+//             setImageURL(res.data.background_image);
+//             setDescript(res.data.description);
+//             setAdded(res.data.added);
+//             setDevelopers(res.data.developers.name);
+//         })
+//         .catch(err=>{
+//             return <p>There has been an error in the fetch. Please resolve.</p>
+//         })
+//     }
+
+//     export default Axios;
