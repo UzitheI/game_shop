c3a0851c1982c93c5e844445c832e8e4056f289d
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-09 22:16:20 +0545

    fixed link and path

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,9 @@ import './index.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Add from "../src/components/Add.jsx";
 import PlayDice from "./components/PlayDice.jsx";
+import Last30days from './components/Last30days.jsx';
+import Top30oftheMonth from './components/Top30oftheMonth.jsx';
+import Alltime from './components/Alltime.jsx';
 
 const router=createBrowserRouter([
   {path:"/",
@@ -21,7 +24,20 @@ const router=createBrowserRouter([
   {
     path:"header",
     element:<App/>
+  },
+  {
+    path:"Last30days",
+    element:<Last30days/>
+  },
+  {
+    path:"Top30oftheMonth",
+    element:<Top30oftheMonth/>,
+  },
+  {
+    path:"Alltime",
+    element:<Alltime/>
   }
+
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
