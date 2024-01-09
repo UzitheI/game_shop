0afde7d80c70ba17cfdbb664e67e3b6daa5d7751
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-09 22:14:43 +0545

    finished build

--- frontend/src/components/PlayDice.jsx
+++ frontend/src/components/PlayDice.jsx
@@ -11,7 +11,9 @@ const PlayDice=()=>{
     const [id,setId] =useState(null);
     const [descript,setDescript]=useState("Please click the 'New GAme' button to search a game.");
     const [added, setAdded]=useState(null);
-    const [developers, setDevelopers]=useState();
+    const [developers, setDevelopers]=useState('');
+    const [showMore, setshowMore]=useState(false);
+
 
     function getRandomInt(min, max) {
         min = Math.ceil(min);
@@ -23,6 +25,7 @@ const PlayDice=()=>{
             id=getRandomInt(100,4000);
         }
         setId();
+        setshowMore(false);
 
         axios.get(`https://rawg.io/api/games/${id}?token&key=${apiKey}`)
         .then(res=>{
@@ -31,34 +34,39 @@ const PlayDice=()=>{
             setImageURL(res.data.background_image);
             setDescript(res.data.description);
             setAdded(res.data.added);
-            setDevelopers(res.data.developers.name);
+            setDevelopers(res.data.developers[0].name);
         })
         .catch(err=>{
             return <p>There has been an error in the fetch. Please resolve.</p>
         })
     }
+    let maxLength =descript.length;
+    console.log(maxLength);
+    const truncatedText =showMore?descript:descript.slice(0,300);
     return(
         <div className="bg-black text-white bg-full bg-repeat  ">
             <Header/>
             <div className="mt-8">
             <div className="flex justify-around ">
 
-            <p className="flex justify-center text-4xl font-bold">{name}</p>
+            <p className="flex justify-center text-4xl font-bold">{name} ' <p className="bg-black text-white">{developers} '</p>
+</p>
             <button onClick={handleButton} className="bg-red-950 p-4 rounded-3xl ">New game</button>
             </div>
             <div className="flex justify-center ">
 
             <button className="flex justify-center mt-3  p-6 bg-white text-black rounded-3xl hover:bg-black hover:text-white" >Add to cart</button>
             </div>
-            <div className="flex p-28 justify-between h-auto">
+            <div className="flex p-28 justify-between h-30">
 
-            <img src={imageURL} alt="" className=" h-fixed w-1/2 rounded-3xl pop-out " />
-            <p className="w-2/5 bg-white text-black bg-opacity-55 p-8 rounded-3xl pop-out">{descript}</p>
+            <img src={imageURL} alt="" className=" h-30 h-fit w-1/2 rounded-3xl pop-out " />
+            <p className="w-2/5 bg-white text-black bg-opacity-55 p-8 rounded-3xl pop-out">{truncatedText}
+            
+            {!showMore && truncatedText.length<descript.length &&(
+                <button onClick={()=>setshowMore(true)} className="text-blue-500 hover:underline cursor-pointer flex">Read More</button>
+            )}</p>
             </div>
-            <h1 className="flex justify-center text-lg font-bold mb-4">Number of people that have added:{added}</h1>
-            <h2>Developers</h2>
-            <p>{developers}</p>
-
+            <h1 className="flex justify-center text-lg font-bold align-middle">Number of people that have added:{added}</h1>
             </div>
         </div>
     )
