8aca38af1d7d00979b2a862a13a24a97f77f407f
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-11 21:40:57 +0545

    changed the number of games displayed

--- frontend/src/components/TrendingGames.jsx
+++ frontend/src/components/TrendingGames.jsx
@@ -9,7 +9,7 @@ function TrendingGames({gameList}) {
     <h2 className='font-bold text-[30px] text-white'>Trending Games</h2>
 
     <div className='hidden md:grid md:grid-cols-3 lg:grid-cols-4 gap-3'>
-        {gameList.map((item,index)=> (
+        {gameList.map((item,index)=>index<4&&(
             <div className=''>
                 <img src={item.background_image} alt="" className='h-[270px] rounded-t-lg object-cover pop-out hover:opacity-50 cursor-pointer' />
                 <h2 className='text-white text-[20px] font-bold bg-[#76a8f75e] rounded-b-lg p-2 '>{item.name}</h2>
