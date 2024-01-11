42e6d9649ad2bd50d6ff59e746d20c9bf5cdb567
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-11 14:09:13 +0545

    added trending games section

---
+++ frontend/src/components/TrendingGames.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+
+function TrendingGames({gameList}) {
+    useEffect(()=>{
+        // console.log(gameList);
+    })
+  return (
+    <div className='mt-5 hidden md:block'>
+    <h2 className='font-bold text-[30px] text-white'>Trending Games</h2>
+
+    <div className='hidden md:grid md:grid-cols-3 lg:grid-cols-4 gap-3'>
+        {gameList.map((item,index)=> (
+            <div className=''>
+                <img src={item.background_image} alt="" className='h-[270px] rounded-t-lg object-cover pop-out hover:opacity-50 cursor-pointer' />
+                <h2 className='text-white text-[20px] font-bold bg-[#76a8f75e] rounded-b-lg p-2 '>{item.name}</h2>
+            </div>
+        ))}
+    </div>
+    </div>
+  )
+}
+
+export default TrendingGames
