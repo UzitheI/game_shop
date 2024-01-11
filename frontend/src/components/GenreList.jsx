0e4d28804ebb67dcdbf7d3f7be7e32554c609f5c
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-11 21:39:03 +0545

    improv

--- frontend/src/components/GenreList.jsx
+++ frontend/src/components/GenreList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useEffect } from 'react';
 import GlobalAPI from '../Services/GlobalAPI';
 import { useState } from 'react';
-function GenreList() {
+function GenreList({genereId,selectedGenresName}) {
     const [genreList,setGenreList]=useState([]);
     const [activeIndex,setActiveIndex]=useState(0);
     useEffect(()=>{
@@ -19,7 +19,10 @@ function GenreList() {
     <div>
         <h2 className='text-[30px] '>Genre</h2>
         {genreList.map((item,index)=>(
-            <div onClick={()=>setActiveIndex(index)} className={`flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-500 font-bold group rounded-lg} ${activeIndex===index?'bg-gray-300 text-black rounded-lg ':null}`}>
+            <div onClick={()=>{setActiveIndex(index);genereId(item.id);
+            selectedGenresName(item.name)}}
+            
+            className={`flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-500 font-bold group rounded-lg} ${activeIndex===index?'bg-gray-300 text-black rounded-lg ':null}`}>
                 <img src={item.image_background} className='w-[40px] h-[40px] object-cover rounded-lg group-hover:scale-105 transition-all ease-out' alt="" />
                 <h3 className='group-hover:
                 font-bold transition-all ease-out duration-300'>{item.name}</h3>
